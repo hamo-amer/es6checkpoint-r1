3223674fc4a3808fe6cb31386b3186ef91f5c890
Mohamed <mohamedtaher.benarbia@gmail.com>
2020-07-20 17:45:34 +0200

    checkpoint es6

---
+++ index.js
@@ -0,0 +1,18 @@
+let pets=[
+    { name: "Max",type: "dog",bornOn: 2018 },
+    { name: "Angel",type: "cat",bornOn: 2015},
+    { name: "Jasper",type: "dog",bornOn: 2016}
+  ];
+  const getAge=pet=> new Date().getFullYear()-pet.bornOn;
+  
+  let petsWithAge=[];
+  pets.map(pet=>{pet.age=getAge(pet) ;
+  petsWithAge.push(pet);
+  return petsWithAge; })
+  console.log(petsWithAge);
+  
+  let dogs=pets.filter(pet=>{ return pet.type=="dog";});
+  console.log(dogs);
+  
+  let jasper=pets.find(pet=>{return pet.name=="Jasper"});
+  console.log(`Jasper is ${jasper.age} years old`);
